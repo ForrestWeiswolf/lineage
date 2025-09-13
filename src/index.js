a9d4e9e279b4fc688f016f0f0671ad39ab1215de
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-09-13 17:45:06 -0400

    Add events log to html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import * as d3 from 'd3'
 import runHistory from './lineage.js'
 import createTreeChart from './createTreeChart.js'
 
-const { familyTree } = runHistory(445)
+const { familyTree, events, allPeople } = runHistory(445)
+
+const eventsList = document.createElement('div')
+events.forEach(event => {
+  const element = document.createElement('p')
+  element.textContent = event
+  eventsList.appendChild(element)
+})
+
 document.getElementById('container')
   .appendChild(createTreeChart(familyTree))
+
+document.getElementById('container')
+  .appendChild(eventsList)
