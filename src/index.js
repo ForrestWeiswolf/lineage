5e78e55cd18200ab99e228b9fb07684ae2806f5f
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-09-13 21:40:43 -0400

    Improve layout

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import createTreeChart from './createTreeChart.js'
 
 const { familyTree, events, allPeople } = runHistory(445)
 
-const eventsList = document.createElement('div')
+const eventsList = document.getElementById('events')
+
 events.forEach(event => {
   const element = document.createElement('p')
   element.textContent = event
   eventsList.appendChild(element)
 })
 
-document.getElementById('container')
+const livingList = document.getElementById('living-people')
+allPeople.filter(p => p.alive).forEach(person => {
+  const element = document.createElement('p')
+  element.textContent = person.toString()
+  livingList.appendChild(element)
+})
+
+document.getElementById('tree-container')
   .appendChild(createTreeChart(familyTree))
 
-document.getElementById('container')
+document.getElementById('info-container')
   .appendChild(eventsList)
+
+  document.getElementById('info-container')
+  .appendChild(livingList)
