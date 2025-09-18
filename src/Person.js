fa85453fb7a960b07e14f0f078ed7407d29d7985
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-09-18 11:25:13 -0400

    Extract Person class to own file

---
+++ src/Person.js
@@ -0,0 +1,145 @@
+import generateName from './generateName'
+
+class Person {
+  constructor(
+    parent,
+    circleMax,
+    name,
+    sex,
+    xp = 1,
+    isMonarch = false,
+    age = 0,
+    children = [],
+    married = false,
+    alive = true,
+    maxLevel = Math.max(Math.min(Math.ceil(Math.pow(Math.random(), 1.8) * 17) + circleMax, 20), 3)
+  ) {
+    this.parent = parent
+    this.circleMax = circleMax
+    this.name = name
+    this.xp = xp
+    this.age = age
+    this.children = children
+    this.married = married
+    this.alive = alive
+    this.sex = sex
+    this.isMonarch = isMonarch
+    this.maxLevel = maxLevel
+  }
+
+  title() {
+    if (!this.parent) {
+      return null
+    } else if (this.isMonarch) {
+      return this.sex === 'M' ? 'King' : 'Queen'
+    } else if (this.parent.isMonarch || this.circle() >= 5) {
+      return this.sex === 'M' ? 'Prince' : 'Princess'
+    } else {
+      return this.sex === 'M' ? 'Lord' : 'Lady'
+    }
+  }
+
+  shortName() {
+    return this.name.split(' ')[0]
+  }
+
+  level() {
+    let xp = this.xp
+    let lvl = 0
+
+    while (xp > lvl * 1000) {
+      xp -= lvl * 1000
+      lvl++
+    }
+
+    return lvl
+  }
+
+  circle() {
+    return Math.min(this.circleMax, Math.floor(this.level() / 2))
+  }
+
+  infoString() {
+    return `age ${this.age}, circle ${this.circle()}, lvl ${this.level()}, max circle ${this.circleMax}`
+  }
+
+  toString() {
+    return `${this.title() ? this.title() + ' ' : ''}${this.name} (${this.infoString()})`
+  }
+
+  willMarry() {
+    return !this.married && this.age > 30 && this.age < 180 && (
+      (this.children.length === 0 && Math.random() < 0.2)
+      || (this.children.length !== 0 && Math.random() < 0.01)
+    )
+  }
+
+  willDie() {
+    return Math.random() < Math.pow(this.age / (200 + this.circleMax * 10), 15)
+      && (!this.isMonarch || Math.random() < .5)
+  }
+
+  willHaveChild() {
+    return this.married
+      && (this.age < (this.sex === 'M' ? 160 : 120))
+      && (Math.random() > (this.age / (180))) && (Math.random() > 0.96)
+      && (Math.random() > (this.children.length / 10))
+  }
+
+  update(year, events, root) {
+    // Note: doesn't handle royal inheritance; that's done elsewhere
+
+    let { married, alive, xp, age } = this
+    let children = this.children.map(child => child.update(year, events, root))
+    if (this.alive) {
+      age += 1
+
+      if (this.willMarry()) {
+        married = true
+        events.push(`In year ${year}, ${this.title()} ${this.name} got married at age ${this.age}`)
+      }
+
+      if (this.married && (Math.random() < Math.pow(this.age / (200 + this.circleMax * 10), 15))) {
+        married = false
+        events.push(`In year ${year}, ${this.title()} ${this.name}'s spouse died`)
+      }
+
+      if (this.willDie()) {
+        alive = false
+        events.push(`In year ${year}, ${this.toString()} died`)
+      }
+
+      if (this.willHaveChild()) {
+        const childCircleMax = Math.random > 0.85 ? this.circleMax - 2 : this.circleMax - 1
+        const sex = Math.random() < 0.5 ? 'M' : 'F'
+        const child = new Person(this, Math.max(childCircleMax, 0), generateName(sex, root), sex)
+        children.push(child)
+        events.push(`In year ${year}, ${this.toString()} had a child: ${child.name}`)
+      }
+
+      if (this.level() < this.maxLevel) {
+        xp += Math.min(this.age / 100, 0.01) * 360
+        xp += this.circleMax / 10 * 360
+        xp += this.maxLevel + (this?.parent?.level ? this.parent.level() : 20)
+        if (this.parent?.isMonarch) {
+          xp += 100
+        }
+      }
+    }
+
+    const updated = new Person(this.parent, this.circleMax, this.name, this.sex,
+      xp,
+      this.isMonarch,
+      age,
+      children,
+      married,
+      alive,
+      this.maxLevel
+    )
+
+    updated.children.forEach(c => c.parent = updated)
+    return updated
+  }
+}
+
+export default Person
