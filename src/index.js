8bcd585e9ff1fea10825d068991e7611f5ac9605
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-10-05 12:02:21 -0400

    Put history length on a settings object

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import runHistory from './lineage.js'
 import createTreeChart from './createTreeChart.js'
 import populateInfoContainer from './populateInfoContainer.js'
 
-const HISTORY_LENGTH = 445
-const { stateByYear, eventsByYear } = runHistory(HISTORY_LENGTH)
+const settings = { historyLength: 500 }
+const { stateByYear, eventsByYear } = runHistory(settings.historyLength)
 
 const yearSelect = document.getElementById('year-select')
-let currentYear = HISTORY_LENGTH - 1
-for (let i = 0; i < HISTORY_LENGTH; i++) {
+let currentYear = settings.historyLength - 1
+
+for (let i = 0; i < settings.historyLength; i++) {
   const option = document.createElement('option')
   option.value = i
   option.textContent = i
